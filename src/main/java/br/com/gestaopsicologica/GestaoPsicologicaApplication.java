40829540c44c1ad9c7750aba7cb6f3ed0bbb42dd
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-11-10 20:09:26 -0300

    first commit

---
+++ src/main/java/br/com/gestaopsicologica/GestaoPsicologicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaopsicologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPsicologicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPsicologicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaopsicologica/GestaoPsicologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaopsicologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPsicologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
