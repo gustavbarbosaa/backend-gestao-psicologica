c2e9700e5a4b4fe2d367bd73085153dd63f5fbe7
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-02-11 20:25:50 -0300

    feat: cria classe de entidade para tipo de atendimento

---
+++ src/main/java/br/com/gestaopsicologica/domain/TipoAtendimento.java
@@ -0,0 +1,41 @@
+package br.com.gestaopsicologica.domain;
+
+import br.com.gestaopsicologica.enums.TipoAtendimentoEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TipoAtendimento implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 6336991295187594333L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private TipoAtendimentoEnum nome;
+
+    @Column(nullable = false)
+    private BigDecimal valorPadraoTipoAtendimento;
+
+    private Boolean ativo;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp
+    private LocalDateTime alteradoEm;
+}
