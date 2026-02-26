7c08c4d92527ec5bcccb587986d126128f6ceae1
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-02-25 23:50:12 -0300

    feat: ajusta aplicação para ficar configurada no timezone de fortaleza

--- src/main/java/br/com/gestaopsicologica/GestaoPsicologicaApplication.java
+++ src/main/java/br/com/gestaopsicologica/GestaoPsicologicaApplication.java
@@ -1,10 +1,18 @@
 package br.com.gestaopsicologica;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GestaoPsicologicaApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Ameria/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoPsicologicaApplication.class, args);
