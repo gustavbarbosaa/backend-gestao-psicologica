a75cd4bbde2c2196fd750a8450f77b0aa8b1619f
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-05-23 15:35:26 -0300

    feat: adiciona validacao no cadastro de usuarios

--- src/main/java/br/com/gestaopsicologica/DTO/requests/CadastroRequest.java
+++ src/main/java/br/com/gestaopsicologica/DTO/requests/CadastroRequest.java
@@ -1,4 +1,14 @@
 package br.com.gestaopsicologica.DTO.requests;
 
-public record CadastroRequest(String nome, String email, String senha) {
+import jakarta.validation.constraints.*;
+
+public record CadastroRequest(
+        @NotBlank(message = "O nome é obrigatório!")
+        String nome,
+        @NotBlank @Email(message = "O email precisa ser válido!")
+        String email,
+        @Size(min = 6, message = "A senha deve possuir no mínimo  6 caracteres!")
+        @NotBlank(message = "A senha é obrigatória!")
+        String senha) {
+
 }
