e45d4cd59b932f956a9d6e65752c7156a36e8320
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-05-23 15:41:22 -0300

    feat: adiciona lista de permissoes no jwt

--- src/main/java/br/com/gestaopsicologica/config/SecurityFilter.java
+++ src/main/java/br/com/gestaopsicologica/config/SecurityFilter.java
@@ -6,13 +6,15 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -21,7 +23,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private final TokenConfig tokenConfig;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         Cookie[] cookies = request.getCookies();
 
         if (cookies != null) {
@@ -32,9 +34,12 @@ public class SecurityFilter extends OncePerRequestFilter {
 
                     if  (optionalUsuario.isPresent()) {
                         JWTUsuarioData usuario = optionalUsuario.get();
+                        List<SimpleGrantedAuthority> authorities = usuario.authorities().stream()
+                                .map(SimpleGrantedAuthority::new)
+                                .toList();
 
                         UsernamePasswordAuthenticationToken authenticationToken =
-                                new UsernamePasswordAuthenticationToken(usuario.id(), null, Collections.emptyList());
+                                new UsernamePasswordAuthenticationToken(usuario.id(), null, authorities);
 
                         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     }
