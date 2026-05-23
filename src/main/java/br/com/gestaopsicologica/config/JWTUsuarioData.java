979cc5d0ecf27203a0c6cbf696b342a666a5f1e9
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-05-23 15:40:41 -0300

    feat: adiciona lista de permissoes no jwt

--- src/main/java/br/com/gestaopsicologica/config/JWTUsuarioData.java
+++ src/main/java/br/com/gestaopsicologica/config/JWTUsuarioData.java
@@ -2,8 +2,9 @@ package br.com.gestaopsicologica.config;
 
 import lombok.Builder;
 
+import java.util.List;
 import java.util.UUID;
 
 @Builder
-public record JWTUsuarioData(UUID id, String email) {
+public record JWTUsuarioData(UUID id, String email, List<String> authorities) {
 }
