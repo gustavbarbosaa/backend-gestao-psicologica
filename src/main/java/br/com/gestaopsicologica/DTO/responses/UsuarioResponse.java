98f24c390c7624afc5066efb27bc3d33c35e5365
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-05-28 20:05:51 -0300

    remove: remove permissoes de retorno de login

--- src/main/java/br/com/gestaopsicologica/DTO/responses/UsuarioResponse.java
+++ src/main/java/br/com/gestaopsicologica/DTO/responses/UsuarioResponse.java
@@ -1,13 +1,11 @@
 package br.com.gestaopsicologica.DTO.responses;
 
 import br.com.gestaopsicologica.domain.Usuario;
-import org.springframework.security.core.GrantedAuthority;
 
-import java.util.List;
 import java.util.UUID;
 
-public record UsuarioResponse(UUID id, String nome, String email, List<String> permissoes, Boolean ativo) {
+public record UsuarioResponse(UUID id, String nome, String email, Boolean ativo) {
     public UsuarioResponse(Usuario usuario) {
-        this(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList(), usuario.getAtivo());
+        this(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getAtivo());
     }
 }
