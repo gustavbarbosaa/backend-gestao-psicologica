8f93f77746b2ef2d146cf1743651a4f0a7eaea9e
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-06-17 21:12:39 -0300

    fix: ajusta obrigatoriedade de email no cadastro de paciente

--- src/main/java/br/com/gestaopsicologica/DTO/requests/PacienteRequest.java
+++ src/main/java/br/com/gestaopsicologica/DTO/requests/PacienteRequest.java
@@ -2,7 +2,5 @@ package br.com.gestaopsicologica.DTO.requests;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.util.UUID;
-
-public record PacienteRequest(@NotNull String nome, String email, String telefone, UUID profissionalId) {
+public record PacienteRequest(@NotNull String nome, String email, @NotNull String telefone) {
 }
